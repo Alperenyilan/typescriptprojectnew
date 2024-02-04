10fba6c0b5d9c19d3b30b0064bdf301edf2f521b
Alperen Yılan <49797242+Alperenyilan@users.noreply.github.com>
2024-02-04 04:32:33 +0300

    css düzenlemeleri navbar yapımı

--- app/component/navbar/Navbar.tsx
+++ app/component/navbar/Navbar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import CardCount from "./CardCount";
 import Search from "./Search";
 import Logo from "./Logo";
@@ -7,12 +6,12 @@ import User from "./User";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="flex items-center justify-between gap-3 md:gap-10 px-3 md:px:10 h-16 bg-orange-600 text-slate-100">
       <Logo />
-      <HamburgerMenu />
-      <CardCount />
       <Search />
+      <CardCount />
       <User />
+      <HamburgerMenu />
     </div>
   );
 };
